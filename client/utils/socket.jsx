4800bfe9a9e42807d437b62e54edbbf2ceb04f61
mohanadkandil <mohanadmkandil@gmail.com>
2022-09-23 17:59:05 +0200

    feat: add socket init utility function

---
+++ client/utils/socket.jsx
@@ -0,0 +1,12 @@
+import { io } from "socket.io-client"
+
+export const initSocketClient = async () => {
+  let url = process.env.SOCKET_URL
+  const options = {
+    "force new connection": true,
+    reconnectionAttempt: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
+  }
+  return io(utl, options)
+}
